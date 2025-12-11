91cd299457fe00b65e4df49a2ec0a5467bb05e58
Eatoes <development@eatoes.com>
2025-12-11 10:40:16 +0530

    added details

---
+++ config.js
@@ -0,0 +1,14 @@
+window.SITE_CONFIG = {
+  title: 'Marquee Club',
+  tradeName: 'Marquee Club',
+  legalName: 'EMBERWAVE HOSPITALITY PRIVATE LIMITED',
+  phoneNumber: '8374064840',
+  gstNumber: '36AAJCE0989J1Z5',
+  address: '5th floor, H.No. 2-37/9,10,11,16,17,18, Gachibowli Road, Ancient Stone Flames, Vinayak Nagar, Hyderabad, Telangana, 500032',
+  openingHours: {
+    monFri: 'MON - FRI: 12PM - 10PM',
+    saturday: 'SATURDAY: 12PM - 12AM',
+    sunday: 'SUNDAY: 12PM - 11PM'
+  },
+  facebookDomainVerification: 'hn1n4vu48sabfbp7v2bihdvltkxtz1'
+};
